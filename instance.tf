e211f8ce3b5c1653c9aa3a03abac8d59a719e047
Vishal Sharma <vishal.sharma@Vishals-MacBook-Pro.local>
2023-06-12 19:13:46 +0530

    outputting attributes

--- instance.tf
+++ instance.tf
@@ -1,7 +1,14 @@
-resource "aws_instance" "terraform-practise" {
+resource "aws_instance" "terraform_practise" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
   tags = {
-    Name = "terraform-practise"
+    Name = "terraform_practise"
   }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.terraform_practise.private_ip} >> private_ips.txt"
+  }
+}
+
+output "ip" {
+  value = aws_instance.terraform_practise.public_ip
 }
